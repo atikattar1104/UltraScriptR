4e9670e6c0491dcaf874c1b1dc2fe1eb73c6be4e
Atik Shakil Attar <atikattar1104@gmail.com>
2022-09-14 12:06:10 +0530

    Add files via upload

---
+++ Test_UltraScriptr.py
@@ -0,0 +1,68 @@
+from project import unix
+
+from project import powershell
+
+from project import batch
+
+from project import cmd
+
+from project import append
+
+
+def main():
+
+test_unix()
+
+test_powershell()
+
+test_batch()
+
+test_cmd()
+
+test_append()
+
+
+def test_unix():
+
+fname0 = "test"
+
+str0 = "echo hello from pytest"
+
+assert unix(fname0, str0) == True, "Function is working"
+
+
+def test_powershell():
+
+fname1 = "test"
+
+str1 = "echo hello from pytest"
+
+assert powershell(fname1, str1) == True, "Function is working"
+
+
+def test_batch():
+
+fname2 = "test"
+
+str2 = "echo hello from pytest"
+
+assert batch(fname2, str2) == True, "Function is working"
+
+
+def test_cmd():
+
+fname3 = "test"
+
+str3 = "echo hello from pytest"
+
+assert cmd(fname3, str3) == True, "Function is working"
+
+
+def test_append():
+
+fname4 = "test"
+
+str4 = "echo hello from pytest"
+
+assert append(fname4, str4) == True, "Function is working"
+
